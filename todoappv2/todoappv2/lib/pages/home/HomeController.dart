ca9fe834544466a66fcfcde2acbb3a9445777bdc
KiranShaukat <shaukatkiran12@gmail.com>
2022-06-13 17:56:33 +0500

    listtile

--- todoappv2/todoappv2/lib/pages/home/HomeController.dart
+++ todoappv2/todoappv2/lib/pages/home/HomeController.dart
@@ -1,42 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
-
 class HomeController extends StatefulWidget {
   const HomeController({Key? key}) : super(key: key);
-
   @override
   State<HomeController> createState() => _HomeControllerState();
 }
-
 class _HomeControllerState extends State<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-        title: const Text(" ToDo List"),
-        )
-        
        floatingActionButton: FloatingActionButton(
         onPressed: () {},
         backgroundColor: Colors.blue,
         child: const Icon(Icons.add),
-
-       body:Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            child: Text("ALL TASKS"),
-          )
-        ],
         ),
-  
-)
+       body:ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListTile(
+              title: Center(child: Text("Your All Tasks")),
+              subtitle: Center(child: Text("see all tasks at one glance")),
+              onLongPress: (){},
+              tileColor: Colors.amber,
+            ),
+          ),
+        ],
+       ),
+      appBar:AppBar(
+        title: const Text(" ToDo List"),
+        )
+      //  Column(
+      //   mainAxisAlignment: MainAxisAlignment.start,
+      //   crossAxisAlignment: CrossAxisAlignment.stretch,
+      //   children: <Widget>[
+      //     ListTileStyle
+      //   ],
+      //   ), 
       // body: SafeArea( 
       //   ),
-      
-     
     );
   }
 }
